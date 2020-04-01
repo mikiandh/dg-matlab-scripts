5a74338f57447e915e74f212159b15bb32b2a072
Miquel Herrera Clapera <44006095+mikiandh@users.noreply.github.com>
2020-04-01 15:39:59 +0200

    Fix Periodic Boundary Condition
    Fixed a bug that could make the periodic boundary condition
    dependend onn the order in which Riemann fluxes were computed.
    
    Heterogeneous boundaries where only one of the two is Periodic
    now behave as expected.

--- Mesh/Periodic.m
+++ Mesh/Periodic.m
@@ -1,6 +1,12 @@
 classdef Periodic < Boundary
     % Periodic coupling of this boundary with that at the opposite end of 
-    % the mesh.
+    % the mesh. Done by storing both a (deep) copy of the bound element of
+    % the opposite boundary, and a handle to it (for updating purpouses).
+    properties (Access = protected)
+        % Handle to the "mesh-owned" instance of this boundary's ghost
+        % element:
+        oppositeBoundElement
+    end
     methods
         %% Constructor
         function these = Periodic(n)
@@ -16,16 +22,20 @@ classdef Periodic < Boundary
             % of the element adjacent to the domain's opposite boundary.
             if isLeft
                 this.boundElement = elements(1);
-                this.ghostElement = elements(end);
+                this.oppositeBoundElement = elements(end);
+                this.ghostElement = Element(elements(end).basis,[elements(end).xL elements(end).xR]);
             else
                 this.boundElement = elements(end);
-                this.ghostElement = elements(1);
+                this.oppositeBoundElement = elements(1);
+                this.ghostElement = Element(elements(1).basis,[elements(1).xL elements(1).xR]);
             end
         end
         %% Enforce (scalar)
-        function apply_scalar(varargin)
-            % Intentionally does nothing (the ghost element is a handle to
-            % an actual element, which is updated by the solver).
+        function apply_scalar(this,varargin)
+            % Updates the ghost element's states with those of its "real"
+            % counterpart (i.e. the copy of it owned by the mesh).
+            this.ghostElement.states = this.oppositeBoundElement.states;
+            this.ghostElement.interpolateStateAtEdges
         end
     end
 end
