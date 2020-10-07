80f92bc49f8f8d84b67a9542a453b18724530c17
Miquel Herrera Clapera <44006095+mikiandh@users.noreply.github.com>
2020-10-07 18:50:56 +0200

    Fix: Asthana and Jameson's objective function
    Tried and compared 'trapz' with 'adaptive Gauss-Kronrod'; the former
    is good enough.
    
    I decided to integrate from -pi to pi (scaled wavenumbers), to exploit
    'trapz' superaccuracy with periodic fuctions.
    
    Also decided to normalize the result by pi*J (and not J^2, which makes
    no sense).
    
    Confirmed that I get exactly the same objective function that Asthana
    and Jameson (2015) do (fig. 10), if I use interpolation for the I.C. Fourier
    coeffs. However, I cannot reproduce the same improvement factors over DG that
    they report (table 1), despite the optimal correction functions ('c' parameter)
    being (approx.) the same.
    
    I now suspect that they greatly overestimated the improvement of both OESFR
    and OFR over DG; if that is the case, optimal DGIGA turns out to be quite good!
    (I suspect a heavy overestimation of their optimum

--- Experiments/Optimum methods/objFun_Asthana2015.m
+++ Experiments/Optimum methods/objFun_Asthana2015.m
@@ -1,15 +1,12 @@
 function f = objFun_Asthana2015(basis)
-% Objective function a la Asthana & Jameson (2015).
+% Objective function from Asthana & Jameson (2015).
 %
 % I/O
 %  basis: discretization to evaluate
 %  ---
 %  f: cost <-> badness <-> penalty (smaller is better)
 %
-[z,k,e] = basis.getFourierFootprint;
-z(:,k < 0) = [];
-e(:,k < 0) = [];
-k(k < 0) = [];
+[z,k,e] = basis.getFourierFootprint('order','energy');
 f = trapz(k,abs(1 - exp(1i*100*(k - 1i*z))).*e,2);
-f = sum(f)/basis.basisCount^2;
+f = sum(f)/(2*basis.basisCount*pi); % I prefer this normalization
 end
