150f536459a39e7ec4eef3027905a0be26077f0f
Miquel Herrera Clapera <44006095+mikiandh@users.noreply.github.com>
2020-12-07 13:59:36 +0100

    Tweak Cole-Hopf solver test script

--- Experiments/Linear VS nonlinear/testColeHopf.m
+++ Experiments/Linear VS nonlinear/testColeHopf.m
@@ -2,28 +2,45 @@
 
 %% Setup
 nu = 1e-3; % viscosity
-t = 0:.05:4; % sample instants
+t = 0:.05:.75; % sample instants
 x = linspace(-1,1,500); % sample points
-%%% f = @(x) 1+.1*sin(2*pi*x); % initial condition
+%%% f = @(x) 1+.1*sin(8*pi*x); % initial condition
 f = @(x) exp(-9*pi/4*x.^2); % initial condition
 
+%% Plot characteristic curves
+x0 = linspace(x(1),x(end),50);
+xi = f(x0).'.*t + x0.'; % trajectories (row: origin point; column: instant)
+figure
+yyaxis left
+plot(xi,t,'-')
+ylabel('t')
+yyaxis right
+plot(x,f(x),'-');
+ylabel('f')
+xlabel('x')
+
 %% Plot initial condition
+figure
 plot(x,f(x),':k','DisplayName',sprintf('t = %g',0))
 drawnow
 
 %% Compute solution in time
 y = burgersColeHopf(t,x,f,nu,x([1 end]));
+z = Burgers.MOC_robust(t,x,f,x([1 end]));
 
 %% Plot solution in time
 hold on
-h = plot(x,y(1,:),'DisplayName',sprintf('t = %g',t(1)));
+h1 = plot(x,y(1,:),'DisplayName',sprintf('t = %g',t(1)));
+h2 = plot(x,z(1,:),'DisplayName',sprintf('t = %g, inviscid',t(1)));
 hold off
 xlabel('x')
 ylabel('u')
-title(sprintf('Viscosity: %g',nu))
+title(sprintf('Burgers (viscosity: %g); f = %s',nu,func2str(f)))
 legend('Location','Best')
 for i = 2:numel(t)
-    h.YData = y(i,:);
-    h.DisplayName = sprintf('t = %g',t(i));
-    pause(.1)
+    h1.YData = y(i,:);
+    h1.DisplayName = sprintf('t = %g, viscous',t(i));
+    h2.YData = z(i,:);
+    h2.DisplayName = sprintf('t = %g, inviscid',t(i));
+    pause
 end
